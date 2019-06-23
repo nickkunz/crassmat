e165044467489148cf415251bf729e43e1b20a49
Nick Kunz <nick.kunz@columbia.edu>
2019-06-22 23:09:37 -0700

    delete example, change version convention

--- R/CRASSMAT.R
+++ R/CRASSMAT.R
@@ -1,73 +1,48 @@
 #' @title CRASSMAT
-#' @description This package includes CRASSMAT, a function for conditional random sampling out observations in sparse matrices. CRASSMAT takes a given sparse matrix Aij and samples out a single jth value on the condition that the number of jth values within the ith observation is greater than the specified conditional (minimum number of values to remain per ith observation). This process repeats itself until the specified sampling threshold is met. Although CRASSMAT was designed for applications with sparse matrices, it can also be applied to complete matrices or those with missing values. This package is useful for applications where sampling out observed values in a matrix is important for testing the predictive accuracy of data imputation methods, such as matrix factorization or singular value decomposition (SVD).
-#' @keywords matrix, matrix sampling, sparse matrix, conditional random sampling, matrix factorization, svd sampling
-#'
-#' @param sparse_matrix data
-#' @param test_split_thres percent sampled out of matrix from observed values
-#' @param conditional number of observed values in matrix to remain per row
-#'
-#' @return sampled matrix
-#' 
-#' @docType data
-#' @usage data(A)
-#' 
-#' @examples 
-#' data(A)
-#' A_training <- crassmat(sparse_matrix = A,
-#'                        test_split_thres = 0.20,
-#'                        conditional = 1)
-#'
+#' @description This package includes CRASSMAT, a function for conditional random sampling observations in sparse matrices. CRASSMAT takes a given sparse matrix Aij and samples out a single jth value on the condition that the number of jth values within the ith observation is greater than the specified conditional (minimum number of values to remain per ith observation). This process repeats itself until the specified sampling threshold is met. Although CRASSMAT was designed for applications with sparse matrices, it can also be used with complete matrices or those with missing values. This package is useful for applications where sampling out observed values in a matrix is important for testing the predictive accuracy of data imputation methods, such as matrix factorization or singular value decomposition (SVD).
+#' @keywords matrix matrices sampling sparse conditional random imputation
+#' @aliases crassmat
+#' @author Nick Kunz
 #' @export crassmat
-
-## title: CRASSMAT
-## description: conditional random sampling for sprase matrices
-## author: nick kunz <nick.kunz@columbia.edu>
-## created: 04/22/2019
-## updated: 06/20/2019
-
+#' @param data either a sparse matrix, a complete matrix, or a matrix containing missing values
+#' @param sample_thres a non-negative decimal specifying the percentage sampled out of the matrix from observed values 
+#' @param conditional a non-negative integer specifying the number of observed values in matrix to remain per row
+#' @return The returned object is a matrix with x number of values sampled without replacement based on the specified sampling threshold and conditional set by the user.
 ## create crassmat
-crassmat <- function(sparse_matrix, test_split_thres, conditional) {
+crassmat <- function(data, sample_thres, conditional) {
     
     ## duplicate original matrix (test set)
-    sparse_matrix_copy <- sparse_matrix
-    
-    ## install / load progress tracking dependent library
-    if (!require("svMisc"))  
-        install.packages("svMisc")
+    data_copy <- data
     
     ## conditional repeat
     repeat {
         
         ## conditional sampling for loop
-        for (i in 0:nrow(sparse_matrix_copy)) {  ## loop through rows not na
-            if (length(which(!is.na(sparse_matrix_copy[i, ]))) > conditional) {  ## conditional statement
+        for (i in 0:nrow(data_copy)) {  ## loop through rows not na
+            if (length(which(!is.na(data_copy[i, ]))) > conditional) {  ## conditional statement
                 
                 ## conduct single sample removal for rows that meet conditional
-                sparse_matrix_copy[i, ][sample(which(!is.na(sparse_matrix_copy[i, ])), size = 1)] <- NA
+                data_copy[i, ][sample(which(!is.na(data_copy[i, ])), size = 1)] <- NA
 
                 ## conduct progress tracking by percentage 
-                svMisc::progress(i / nrow(sparse_matrix_copy) * 100)
+                svMisc::progress(i / nrow(data_copy) * 100)
                 
                 ## print prompt when completed
-                if(i == nrow(sparse_matrix_copy)) 
+                if(i == nrow(data_copy)) 
                     cat("Complete!")
                 
             } ## close matrix for loop conditional statement
         } ## close matrix for loop through rows not na
         
         ## stop repeat when sampling threshold is met
-        if ((mean(is.na(sparse_matrix_copy)) - mean(is.na(sparse_matrix))) / 
-            mean(!is.na(sparse_matrix)) > test_split_thres) break
+        if ((mean(is.na(data_copy)) - mean(is.na(data))) / 
+            mean(!is.na(data)) > sample_thres) break
         
     } ## close conditional repeat
     
     ## return sampled matrix
-    return(sparse_matrix_copy)
+    return(data_copy)
     
 } ## close function
 
 ## end
-
-A_training <- crassmat(sparse_matrix = A,
-                       test_split_thres = 0.20,
-                        conditional = 1)
