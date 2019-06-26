1232c4a4cd63db4da08a9f3688dfede4bce7a87e
Nick Kunz <nick.kunz@columbia.edu>
2019-06-26 15:27:33 -0700

    upload R file data, version 0.0.5

---
+++ R/A.R
@@ -0,0 +1,9 @@
+#' @title Sparse Matrix A
+#' @description Data for implementing the example given for CRASSMAT.
+#' @docType data
+#' @keywords sparse matrix data
+#' @name A
+#' @usage data(A)
+#' @format A sparse matrix containing 15 columns and 3000 observations
+#' @author Nick Kunz <\email{nick.kunz@columbia.edu}>
+'A'
